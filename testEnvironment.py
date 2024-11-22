4a7cc4d2538d8444260e71d392663e7c2302d654
Breadifies <jomds222@gmail.com>
2024-11-22 05:48:49 +0000

    created baseline structure for actor-critic model

--- testEnvironment.py
+++ testEnvironment.py
@@ -5,11 +5,13 @@ from pyglet.window import key, mouse
 from pyglet import shapes
 from pyglet import gl #graphics drawing done via GPU than CPU, more optimised
 from vehicle_Agent import VehicleAgent, RoadTile
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
 
 
-
-window = pyglet.window.Window(width=1568, height=882, vsync=True)  # vsync, eliminate unnecessary variables
+window = pyglet.window.Window(width=800, height=600, vsync=True)  # vsync, eliminate unnecessary variables
 WINDOW_WIDTH, WINDOW_HEIGHT = window.get_size()
 batch = pyglet.graphics.Batch()
 batch2 = pyglet.graphics.Batch()
@@ -50,41 +52,11 @@ FRICTION = MU * 3 #FRICTION coefficient
 TURNING_FRICTION = MU * 20 #how quickly rotation stops
 CAR_LENGTH = MU*4.2 #4.2 meters, pixel per meter 21:9 ratio standard
 CAR_WIDTH = MU*1.8  #= 1.8 meters to pixels is 24, therfore 1 meter = 13.33 pixels
+SPEED_UP = 1 #speed up simulation
 
 vAgents = [
     VehicleAgent(x=450, y=450, width=CAR_LENGTH, height=CAR_WIDTH, color=(200, 225, 90), batch1=batch, batch2=batch2, isControlled=True),
-    # VehicleAgent(x=300, y=450, width=CAR_LENGTH, height=CAR_WIDTH, color=(50, 225, 30), batch1=batch, batch2=batch2)
-    # VehicleAgent(x=120, y=450, width=38, height=25, color=(214, 125, 67), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=400, y=450, width=42, height=30, color=(50, 225, 30), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=300, y=450, width=42, height=30, color=(50, 225, 30), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=120, y=450, width=38, height=25, color=(214, 125, 67), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=500, y=400, width=45, height=32, color=(100, 150, 200), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=200, y=400, width=40, height=28, color=(255, 0, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=350, y=420, width=44, height=31, color=(0, 255, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=250, y=430, width=41, height=29, color=(0, 0, 255), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=100, y=460, width=39, height=26, color=(255, 255, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=450, y=410, width=43, height=29, color=(0, 255, 255), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=150, y=470, width=37, height=24, color=(255, 0, 255), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=320, y=440, width=46, height=33, color=(128, 128, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=220, y=460, width=40, height=27, color=(128, 0, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=370, y=430, width=42, height=28, color=(0, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=270, y=450, width=44, height=30, color=(0, 0, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=180, y=480, width=41, height=29, color=(128, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=420, y=420, width=45, height=31, color=(0, 128, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=320, y=460, width=43, height=28, color=(128, 0, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=370, y=440, width=46, height=33, color=(128, 128, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=270, y=460, width=40, height=27, color=(128, 0, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=220, y=430, width=42, height=28, color=(0, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=170, y=450, width=44, height=30, color=(0, 0, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=420, y=480, width=41, height=29, color=(128, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=320, y=420, width=45, height=31, color=(0, 128, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=370, y=460, width=43, height=28, color=(128, 0, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=270, y=440, width=46, height=33, color=(128, 128, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=220, y=460, width=40, height=27, color=(128, 0, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=170, y=430, width=42, height=28, color=(0, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=420, y=450, width=44, height=30, color=(0, 0, 128), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=320, y=480, width=41, height=29, color=(128, 128, 0), batch1=batch, batch2=batch2),
-    # VehicleAgent(x=370, y=420, width=45, height=31, color=(0, 128, 128), batch1=batch, batch2=batch2)
+    VehicleAgent(x=300, y=450, width=CAR_LENGTH, height=CAR_WIDTH, color=(50, 225, 30), batch1=batch, batch2=batch2)
 ]
 #/////////////////////////////////////////////////////////////////////////////////////////#////////////////////////////////////////////////////////////////////////////////////////       
 ROAD_WIDTH = CAR_WIDTH * 2 #should be roughly DOUBLE a saloon width
@@ -95,7 +67,8 @@ roads.append(road)
 
 #dt is the time elapsed since last call
 def update(dt): #ensuring consistent framerate and game logic to be frame-rate indepependent
-    
+    dt = dt * SPEED_UP #speed up simulation
+
     global keys_pressed, ACCELERATION, DECELERATION, TURNING_ACCEL, FRICTION, TURNING_FRICTION
     for a in vAgents: #looping through every vehicle agent
         left, right, down, up = a.current_direction
@@ -159,24 +132,10 @@ def update(dt): #ensuring consistent framerate and game logic to be frame-rate i
         a.updateLines()
         a.changeAnchor(a.turning_anchor_x, a.turning_anchor_y)
 
-def update_direction(dt): #operates on longer timesteps than animation update
-    for a in vAgents:
-        if not a.isControlled:
-            a.updateDirection()
-        currentState = [a.shape.x, a.shape.y, a.deg_angle, a.velocity, a.turning_speed, a.lineLengths]
-        print(currentState)
-
-
 def update_user_direction(dt):
     for a in vAgents:
         if a.isControlled:
             a.current_direction = keys_pressed
-
-
-pyglet.clock.schedule_interval(update_direction, 0.3)  # Update direction every xx seconds
-pyglet.clock.schedule(update)
-pyglet.clock.schedule(update_user_direction)
-
 
 @window.event
 def on_draw():
@@ -185,6 +144,134 @@ def on_draw():
     roadBatch.draw()
     batch2.draw() #vision lines
     batch.draw()
+
+def update_direction(dt): #operates on longer timesteps than animation update
+    global current_len
+    current_len = current_len + 1
+    for a in vAgents:
+        currentState = [a.shape.x, a.shape.y, a.deg_angle, a.velocity, a.turning_speed, a.lineLengths[0], a.lineLengths[1], a.lineLengths[2]]
+        #store current state
+        global_states.append(currentState)
+        action = pick_action(currentState) #process state and get action out of it
+        if not a.isControlled:
+            a.updateDirection(action) #do the determined action
+        global_actions.append(action)
+        global_rewards.append(0) #no rewards implemented yet
+
+    print(current_len)
+    if current_len >= MAX_EP_LENGTH:
+        update_episode() #get cumulative rewards
+        exit() #should reset the environment back to the beginning
+
+
+pyglet.clock.schedule_interval(update_direction, 0.3/SPEED_UP)  # Update direction every xx seconds
+pyglet.clock.schedule(update)
+pyglet.clock.schedule(update_user_direction)
+
+
+
+
+
+
+
+
+
+
+
+#ACTOR_CRITIC_NETWORK.py
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+ACTOR_INPUTS = 8 #based on test environment
+class ActorNet(nn.Module): #our ACTOR network
+    def __init__(self, hidden_dim=16):
+        super().__init__()
+        self.hidden = nn.Linear(ACTOR_INPUTS, hidden_dim) #input layer has 8 units, hidden layer has 16 units
+        self.output = nn.Linear(hidden_dim, 5) #output layer has 2 units
+    def forward(self, s): #the NN forward pass
+        outs = self.hidden(s)
+        outs = F.relu(outs) #applies ReLU activation
+        logits = self.output(outs) #computes logits -> passed through e.g. a softmax function to convert to probabilities
+        return logits
+
+class ValueNet(nn.Module): #our VALUE network
+    def __init__(self, hidden_dim=16):
+        super().__init__()
+        self.hidden = nn.Linear(ACTOR_INPUTS, hidden_dim)
+        self.output = nn.Linear(hidden_dim, 1) #1 unit to predict value of the state
+    def forward(self, s):
+        outs = self.hidden(s)
+        outs = F.relu(outs)
+        value = self.output(outs)
+        return value
+
+#instantiate the actor and value networks and move to GPU if available (CPU otherwise)
+actor_func = ActorNet().to(device) 
+value_func = ValueNet().to(device)
+
+def pick_action(state):
+    with torch.no_grad():
+        state_batch = np.expand_dims(state, axis=0) #add extra dimension to state for input to NN
+        state_batch = torch.tensor(state_batch, dtype=torch.float).to(device) #expanded state converted to PyTorch tensor and moved to GPU or CPU
+        logits = actor_func(state_batch) #pass state tensor through actor network to get logits
+        logits = logits.squeeze(dim=0)
+        probs = F.softmax(logits, dim=-1) #convert logits to probabilities
+        a = torch.multinomial(probs, num_samples=1) #sample an action from probabilities
+        return a.tolist()[0] #return the action
+        #recall that action space is 4 so will be from 0 to 3
+        #[0, 0, 0, 0] = 0
+        #[1, 0, 0, 0] = 1
+        #[0, 1, 0, 0] = 2
+        #[0, 0, 1, 0] = 3
+        #[0, 0, 0, 1] = 4
+
+reward_records = []
+optActor = torch.optim.AdamW(actor_func.parameters(), lr=0.001) #set up AdamW optimisers for Value & Actor networks
+optCritic = torch.optim.AdamW(value_func.parameters(), lr=0.001) #these are the OPTIMISER ALGORITHMS to find best weights
+
+global_states = []
+global_actions = []
+global_rewards = []
+MAX_EP_LENGTH = 50
+current_len = 0
+gamma = 0.99 #discount factor for future rewards (later timesteps)
+
+def update_episode():
+    #get cumulative rewards by iterating backwards through rewards and applying discount factor
+    global global_states, global_actions, global_rewards
+    cum_rewards = np.zeros_like(global_rewards)
+    reward_len = len(global_rewards)
+    for j in reversed(range(reward_len)):
+        cum_rewards[j] = global_rewards[j] + (cum_rewards[j+1]*gamma if j+1 < reward_len else 0)
     
+    #Train (optimise parameters)
+    #optimisng value LOSS (critic)
+    optCritic.zero_grad()
+    global_states = torch.tensor(global_states, dtype=torch.float).to(device)
+    cum_rewards = torch.tensor(cum_rewards, dtype=torch.float).to(device)
+    values = value_func(global_states)
+    values = values.squeeze(dim=1)
+    vf_loss = F.mse_loss(
+        values,
+        cum_rewards,
+        reduction="none")
+    vf_loss.sum().backward()
+    optCritic.step()
+
+    #otimising policy loss (Actor)
+    with torch.no_grad():
+        values = value_func(global_states)
+    optActor.zero_grad()
+    global_actions = torch.tensor(global_actions, dtype=torch.int64).to(device)
+    advantages = cum_rewards - values
+    logits = actor_func(global_states)
+    log_probs = F.cross_entropy(logits, global_actions, reduction="none")
+    pi_loss = -log_probs * advantages
+    pi_loss.sum().backward()
+    optActor.step()
+    
+    #OUTPUT total rewards in current episode
+    print("Run episode{} with rewards {}".format(0, 0), end="\r")
+    reward_records.append(sum(global_rewards))
+
 
 pyglet.app.run()        
